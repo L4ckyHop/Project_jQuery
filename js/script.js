32e32e689734a946e8dd901f9632abee97a90130
Склизков Артём <L4ckyHop>
2025-06-14 11:48:56 +0500

    добавление базового массива фильмов

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    // Данные фильмов
+    const mockMovies = [
+        {
+            id: 1,
+            title: "Пять Ночей с Фредди",
+            year: 2014,
+            genre: "Ужасы",
+            rating: 8.6,
+            description: "Погрузитесь в страшную атмосферу пиццерии Фредди Фазбера",
+            poster: "img/fnaf.jpg",
+            isFavorite: true
+        },
+        {
+            id: 2,
+            title: "Фильм",
+            year: 2025,
+            genre: "Фантастика",
+            rating: 8.7,
+            description: "Невероятный фильм, который основан на реальных событиях",
+            poster: "img/fil.webp",
+            isFavorite: false
+        }
+    ];
+});
